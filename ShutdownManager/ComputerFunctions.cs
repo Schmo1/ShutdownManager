d43f6d46ea023ad1b11a481a0734bbb043b21578
David Schmoranz <david.schmoranz@outlook.com>
2021-11-01 11:51:29 +0100

    Projektdateien hinzufügen.

---
+++ ShutdownManager/ComputerFunctions.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Hardcodet.Wpf.TaskbarNotification;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ShutdownManager
+{
+    internal enum eTimerZeroActions { Shutdown, Restart, EnergySafe }
+
+
+    class ComputerFunctions
+    {
+
+        //Constanten
+        private const string balloonTipTitle = "ShutdownManager";
+
+        public TaskbarIcon tb = new TaskbarIcon();
+        public Timer timer = new Timer();
+
+
+        //variables
+        private int hours;
+        private int minutes;
+        private int seconds;
+
+        private bool timerHasStarted;
+
+
+        //Properties
+        public TimeSpan TimeLeft { get; set; }
+        public eTimerZeroActions TimerZeroAction {get; set;}
+
+        public int Hours
+        {
+
+            get => hours;
+            set
+            {
+                hours = value;
+                UpdateTimeSpan();
+            }
+        }
+
+
+        public int Minutes
+        {
+            get => minutes;
+            set
+            {
+                minutes = value;
+                UpdateTimeSpan();
+            }
+        }
+
+        public int Seconds
+        {
+            get => seconds;
+            set
+            {
+                seconds = value;
+                UpdateTimeSpan();
+            }
+        }
+
+
+        //Konstruktor
+        public ComputerFunctions()
+        {
+            
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Interval = 1000; //one Second  
+        }
+
+
+
+
+        //Methode
+
+        private void UpdateTimeSpan()
+        {
+            TimeLeft = new TimeSpan(hours,minutes,seconds);
+            
+        }
+
+
+        public void StartTimer()
+        {
+            if (!timerHasStarted)
+            {
+                string message = "Timer has started";
+
+                BalloonIcon icon = BalloonIcon.Info;
+                tb.ShowBalloonTip(balloonTipTitle, message, icon);
+
+                timer.Start();
+                timer.Enabled = true;
+                timerHasStarted = true;
+            }
+
+        }
+
+        public void StopTimer(bool withBalloonTip)
+        {
+            if (timerHasStarted)
+            {
+                if (withBalloonTip)
+                {
+                    string message = "Timer has stopped";
+                    BalloonIcon icon = BalloonIcon.Info;
+                    tb.ShowBalloonTip(balloonTipTitle, message, icon);
+                }
+
+                timer.Enabled = false;
+                timer.Stop();
+
+                timerHasStarted = false;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeLeft = TimeLeft.Subtract(TimeSpan.FromSeconds(1));
+
+            if (TimeLeft.TotalSeconds < 1)
+            {
+                StopTimer(false);
+                TimeLeft = new TimeSpan(0,0,0);
+                TimerAktions();
+            }
+            
+        }
+
+        private void TimerAktions()
+        {
+            if(TimerZeroAction == eTimerZeroActions.Shutdown)
+            {
+                MessageBox.Show("Shutdown", "Invalid action", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+               
+            }
+            else if (TimerZeroAction == eTimerZeroActions.Restart)
+            {
+                MessageBox.Show("Restart");
+            }
+            else if (TimerZeroAction == eTimerZeroActions.EnergySafe)
+            {
+                MessageBox.Show("EnergySafe");
+            }
+            else
+            {
+                MessageBox.Show("No action was selected. Please select some action!", "Invalid action", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+    }
+
+
+
+
+    
+}
---
+++ ShutdownManager/MainWindow.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+
+namespace ShutdownManager
+{
+
+    
+
+    public partial class MainWindow : Window
+    {
+
+        ComputerFunctions pcFunktions = new ComputerFunctions();
+
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            pcFunktions.timer.Tick += OnTimerTick; //Update Timer event
+        }
+
+
+        private void Button_Start(object sender, RoutedEventArgs e)
+        {
+            CheckEmptyUserInput();
+            pcFunktions.StartTimer();
+            
+        }
+        private void Button_Stop(object sender, RoutedEventArgs e)
+        {
+            pcFunktions.StopTimer(true); //With BalloonTip
+        }
+
+        private void HoursTxt_TextChanged(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int hours = Convert.ToInt32(txtHours.Text);
+                if(hours > 23)
+                {
+                    pcFunktions.Hours = 23;
+                    pcFunktions.Minutes = 59;
+                    pcFunktions.Seconds = 59;
+                }
+                else
+                {
+                    pcFunktions.Hours = hours;
+                }
+
+                UpdateTimer();
+            }
+            catch (FormatException ex)
+            {
+                CheckFormatException(ex, txtHours.Text);
+
+            }
+
+
+        }
+
+        private void MinutesTxt_TextChanged(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int minutes = Convert.ToInt32(txtMinutes.Text);
+                if (pcFunktions.Hours > 22 && minutes > 59)
+                {
+                    pcFunktions.Minutes = 59;
+                }
+                else
+                {
+                    pcFunktions.Minutes = Convert.ToInt32(txtMinutes.Text);
+                }
+
+                    
+                UpdateTimer();
+            }
+            catch (FormatException ex)
+            {
+                CheckFormatException(ex, txtMinutes.Text);
+
+            }
+            
+
+
+        }
+        private void SecondsTxt_TextChanged(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int seconds = Convert.ToInt32(txtSeconds.Text);
+                if (pcFunktions.Hours > 22 && pcFunktions.Minutes > 58 && seconds > 59)
+                {
+                    pcFunktions.Seconds = 59;
+                }
+                else
+                {
+                    pcFunktions.Seconds = Convert.ToInt32(txtSeconds.Text);
+                }
+                    
+                UpdateTimer();
+            }
+            catch (FormatException ex)
+            {
+                CheckFormatException(ex, txtSeconds.Text);
+            }
+
+        }
+
+
+
+        private void UpdateTimer()
+        {
+            try
+            {
+                TbTimer.Text = pcFunktions.TimeLeft.ToString(@"hh\:mm\:ss");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid Timespan", "Invalid Format", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void CheckFormatException (FormatException ex, string text)
+        {
+            if (text != null && text != "")
+            {
+                MessageBox.Show(ex.ToString(), "Invalid Format", MessageBoxButton.OK);
+            }
+        }
+
+
+
+        private void CheckEmptyUserInput()
+        {
+            if(txtHours.Text.Length == 0)
+            {
+                txtHours.Text = "00";
+            }
+
+            if (txtMinutes.Text.Length == 0)
+            {
+                txtMinutes.Text = "00";
+            }
+
+            if (txtSeconds.Text.Length == 0)
+            {
+                txtSeconds.Text = "00";
+            }
+        }
+
+
+        private void OnTimerTick(object source, EventArgs args)
+        {
+            UpdateTimer();
+        }
+
+        private void RadioButton_Update(object sender, RoutedEventArgs e)
+        {
+            if ((bool)RadioButton_Shutdown.IsChecked)
+            {
+                pcFunktions.TimerZeroAction = eTimerZeroActions.Shutdown;
+            }
+            else if ((bool)RadioButton_Restart.IsChecked)
+            {
+                pcFunktions.TimerZeroAction = eTimerZeroActions.Restart;
+
+            }
+            else
+            {
+                pcFunktions.TimerZeroAction = eTimerZeroActions.EnergySafe;
+            }
+        }
+    }
+}
